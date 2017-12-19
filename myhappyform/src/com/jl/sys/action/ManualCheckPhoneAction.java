cfba10edbd61d7a9aed6c6046a471aedc99b51d5
zpj <330273931@qq.com>
2017-12-19 16:59:32 +0800

    修改redis库下标
    已完成

--- myhappyform/src/com/jl/sys/action/ManualCheckPhoneAction.java
+++ myhappyform/src/com/jl/sys/action/ManualCheckPhoneAction.java
@@ -25,9 +25,11 @@
 package com.jl.sys.action;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -44,6 +46,8 @@ import com.jl.sys.pojo.CheckInfo;
 import com.jl.sys.pojo.UserInfo;
 import com.jl.sys.service.ManualInfoService;
 import com.jl.sys.service.UserInfoService;
+import com.jl.util.DateHelper;
+import com.jl.util.PingyinTool;
 
 /**
  * @ClassName ManualCheckPhoneAction
@@ -97,6 +101,111 @@ public class ManualCheckPhoneAction extends IAction {
 		}
 		
 	}
+
+
+	@Action(value="jlManualCheckPhoneAction_saveInfoByPhone",
+			results={
+			@Result(type="json", params={"root","jsonData"})})
+	public void saveInfoByPhone(){
+		user =getCurrentUser(request);
+		String id=request.getParameter("id");
+		if(id!=null&&!id.equalsIgnoreCase("")){
+			
+		}else{
+			id=UUID.randomUUID().toString();
+		}
+		String sgxm=request.getParameter("sgxm");
+		String sgqy=request.getParameter("sgqy");
+		String workdate=request.getParameter("workdate");
+		String staffnames=request.getParameter("staffname");
+		String workduringtime=request.getParameter("workduringtime");
+		String overtime=request.getParameter("overtime");
+		String workcontent=request.getParameter("workcontent");
+		String remark=request.getParameter("remark");
+		String departmentname=request.getParameter("departmentname");
+		String departmentcode=PingyinTool.cn2FirstSpell(departmentname);
+		try {
+			boolean flagcn=staffnames.contains("，");
+			boolean flagen=staffnames.contains(",");
+			//判断是否是多人同时输入的
+			if(flagcn||flagen){
+				String[] nameArr=null;
+				if(flagcn){
+					nameArr=staffnames.split("，");
+				}
+				if(flagen){
+					nameArr=staffnames.split(",");
+				}
+				for(int i=0;i<nameArr.length;i++){
+					CheckInfo tmpci=new CheckInfo();
+					tmpci.setId(UUID.randomUUID().toString());
+					tmpci.setStaffname(nameArr[i]);
+					tmpci.setWorkdate(DateHelper.getDateFromString(workdate, "yyyy-MM-dd"));
+					tmpci.setWorkduringtime(Double.valueOf(workduringtime));
+					tmpci.setDepartmentname(departmentname);
+					tmpci.setDepartmentcode(departmentcode);
+					tmpci.setWorkcontent(workcontent);
+					tmpci.setAdddate(new Date());
+//					tmpci.setAddress(cinfo.getAddress());
+					tmpci.setOvertime(Double.valueOf(overtime));	
+					tmpci.setRemark(remark);
+					tmpci.setCreateuserid(user.getId());
+					tmpci.setSgxm(sgxm);
+					tmpci.setSgqy(sgqy);
+					if(user.getIsAdmin().equalsIgnoreCase("1")){
+						//管理员  审核状态改成已审核
+						tmpci.setShenhe("1");
+					}else{
+						//普通人 录入的状态是未审核
+						tmpci.setShenhe("0");
+					}
+					mService.saveInfo(tmpci);
+				}
+				CheckInfo temp=mService.findById(id);
+				if(temp!=null){
+					//编辑的时候 如果是有分隔符 说明是需要分割这条数据  ，在分割保存完这些数据以后要删除之前未分割的数据
+					mService.delInfo(temp.getId());
+				}
+			}else{
+				CheckInfo tmpci=new CheckInfo();
+				tmpci.setId(id);
+				tmpci.setStaffname(staffnames);
+				tmpci.setWorkdate(DateHelper.getDateFromString(workdate, "yyyy-MM-dd"));
+				tmpci.setWorkduringtime(Double.valueOf(workduringtime));
+				tmpci.setDepartmentname(departmentname);
+				tmpci.setDepartmentcode(departmentcode);
+				tmpci.setWorkcontent(workcontent);
+				tmpci.setAdddate(new Date());
+				tmpci.setOvertime(Double.valueOf(overtime));	
+				tmpci.setRemark(remark);
+				tmpci.setCreateuserid(user.getId());
+				tmpci.setSgxm(sgxm);
+				tmpci.setSgqy(sgqy);
+				if(user.getIsAdmin().equalsIgnoreCase("1")){
+					//管理员  审核状态改成已审核
+					tmpci.setShenhe("1");
+				}else{
+					//普通人 录入的状态是未审核
+					tmpci.setShenhe("0");
+				}
+				mService.saveInfo(tmpci);
+			}
+			
+			Map job=new HashMap();
+			job.put("msg",true);
+			this.jsonWrite(job);
+		} catch (Exception e) {
+			Map job=new HashMap();
+			try {
+				job.put("msg",false);
+				this.jsonWrite(job);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * 获取手机当前登录对象信息
 	 * @Title getCurrentUser
