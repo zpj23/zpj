f198a76f21c21091cdebe169c88866758d176920
zpj <330273931@qq.com>
2019-11-12 16:41:26 +0800

    选择排序

--- myhappyform/src/com/test/order/OrderTest.java
+++ myhappyform/src/com/test/order/OrderTest.java
@@ -7,7 +7,9 @@ public class OrderTest {
 	public static void main(String[] args) {
 		int[] arr= {1,4,84,50,49,67,32,18,2,6};
 		int[] arr2=new int[]{1,4,84,50,49,67,32,18,2,6};
-		insert_order(arr,arr2);
+//		insert_order(arr,arr2);
+//		xier_order(arr,arr2);
+		select_order(arr,arr2);
 	}
 	
 	/**
@@ -28,31 +30,66 @@ public class OrderTest {
 		System.out.println("原数组");
 		System.out.println(Arrays.toString(arr2));
 		System.out.println("现数组");
-		for(int m=0;m<arr.length-1;m++){
-//			System.out.println("外循环");
-			for(int n=m+1;n>0;n--){
-//				System.out.println("內循环");
-				if(arr[n-1]>arr[n]){
-					int temp=arr[n-1];
-					arr[n-1]=arr[n];
-					arr[n]=temp;
-					System.out.println(Arrays.toString(arr));
+		for(int i=0;i<arr.length-1;i++){
+			for(int j=i+1;j>0;j--){
+				if(arr[j]<arr[j-1]){
+					int temp=arr[j];
+					arr[j]=arr[j-1];
+					arr[j-1]=temp;
 				}
-				
 			}
-			
 		}
 		
-//		for( int i=0; i<arr.length-1; i++ ) {
-//            for( int j=i+1; j>0; j-- ) {
-//                if( arr[j-1] <= arr[j] )
-//                    break;
-//                int temp = arr[j];      //交换操作
-//                arr[j] = arr[j-1];
-//                arr[j-1] = temp;
-//                System.out.println("Sorting:  " + Arrays.toString(arr));
-//            }
-//        }
+		System.out.println(Arrays.toString(arr));
 		
 	}
+	
+	
+	public static void xier_order(int[] arr,int[] arr2){
+		System.out.println("原数组");
+		System.out.println(Arrays.toString(arr2));
+		System.out.println("现数组");
+	    int gap = 1, i, j, len = arr.length;
+	    int temp;
+	    while (gap < len / 3)
+	        gap = gap * 3 + 1;      // <O(n^(3/2)) by Knuth,1973>: 1, 4, 13, 40, 121, ...
+	    for (; gap > 0; gap /= 3) {
+	        for (i = gap; i < len; i++) {
+	            temp = arr[i];
+	            for (j = i - gap; j >= 0 && arr[j] > temp; j -= gap)
+	                arr[j + gap] = arr[j];
+	            arr[j + gap] = temp;
+	        }
+	    }
+	    System.out.println(Arrays.toString(arr));
+	}
+	
+	
+	/**
+	 * 选择排序
+	 *
+	 * 1. 从待排序序列中，找到关键字最小的元素；
+	 * 2. 如果最小元素不是待排序序列的第一个元素，将其和第一个元素互换；
+	 * 3. 从余下的 N - 1 个元素中，找出关键字最小的元素，重复①、②步，直到排序结束。
+	 *    仅增量因子为1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+	 * @param arr  待排序数组
+	 */
+	public static void select_order(int[] arr,int[] arr2){
+		System.out.println("原数组");
+		System.out.println(Arrays.toString(arr2));
+		System.out.println("现数组");
+		for(int i=0;i<arr.length-1;i++){
+			for(int j=i+1;j<arr.length;j++){
+				if(arr[i]>arr[j]){
+					int temp=arr[j];
+					arr[j]=arr[i];
+					arr[i]=temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+		
+	}
+	
+	
 }
