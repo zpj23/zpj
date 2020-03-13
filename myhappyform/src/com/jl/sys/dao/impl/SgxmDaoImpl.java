fe1823bfc219b84c6a319b5b08b28b75e97dd664
zpj <330273931@qq.com>
2020-03-13 18:05:41 +0800

    添加
    项目工资管理中总工时添加

--- myhappyform/src/com/jl/sys/dao/impl/SgxmDaoImpl.java
+++ myhappyform/src/com/jl/sys/dao/impl/SgxmDaoImpl.java
@@ -82,17 +82,19 @@ public class SgxmDaoImpl extends BaseDao<SgxmInfo> implements SgxmDao {
 		return list;
 	}
 	
-	public void updateMultiInfo(PayrollInfo pi){
+	public synchronized void updateMultiInfo(PayrollInfo pi){
 		try{
 			String firstDay=DateHelper.getFirstdayOfMonth(pi.getYf(), "yyyy-MM");
 			String lastDay=DateHelper.getLastdayOfMonth(pi.getYf(), "yyyy-MM");
 			List<Map> list=invokeCall(firstDay, lastDay,pi.getXm());
-			StringBuffer sql=new StringBuffer(500);
+//			StringBuffer sql=new StringBuffer(500);
 			DecimalFormat df = new DecimalFormat ("#.0");
 			List<Map> alreadylist=this.findMapObjBySql("select * from jl_sgxm_tj_info where yf='"+pi.getYf()+"' and xm='"+pi.getXm()+"' ");
 			StringBuffer nameBuffer;
 			StringBuffer sgxmBuffer;
 			StringBuffer dpBuffer;
+			//平均每个小时的工资
+			float ehgz=(Float.parseFloat(pi.getZgz()))/(Float.parseFloat(pi.getZonggongshi()));
 			boolean flag=false;//判断是否存在新的
 			for(int q=0;q<list.size();q++){
 				flag=false;
@@ -105,10 +107,12 @@ public class SgxmDaoImpl extends BaseDao<SgxmInfo> implements SgxmDao {
 								alreadylist.get(p).get("sgxm").toString().equalsIgnoreCase(sgxmBuffer.toString())&&
 								alreadylist.get(p).get("gd").toString().equalsIgnoreCase(dpBuffer.toString())){
 //							System.out.println("修改现有的项目工资信息");
-							
+							float zgs=(float)list.get(q).get("t5");
 							//计算应发工资=总工时*工价包月
-							double yfgz=((double)list.get(q).get("t5"))*Float.parseFloat(pi.getGjby());
-							this.executeUpdateOrDelete(" update jl_sgxm_tj_info set chuqin='"+list.get(q).get("t3")+"',jiaban='"+list.get(q).get("t4")+"',zonggongshi='"+list.get(q).get("t5")+"' ,lhbt='"+list.get(q).get("t6")+"',gjby='"+pi.getGjby()+"', jbgz='"+pi.getJbgz()+"',yfgz='"+df.format(yfgz)+"'  where yf='"+pi.getYf()+"' and xm='"+nameBuffer+"' and sgxm='"+sgxmBuffer+"' and gd='"+dpBuffer+"' ");
+							float yfgz=zgs*Float.parseFloat(pi.getGjby());
+							//总工资平均分到每个工时里面去的总工时 用每个小时的工资*小时数
+							float zgz =zgs*ehgz;
+							this.executeUpdateOrDelete(" update jl_sgxm_tj_info set chuqin='"+list.get(q).get("t3")+"',jiaban='"+list.get(q).get("t4")+"',zonggongshi='"+list.get(q).get("t5")+"' ,lhbt='"+list.get(q).get("t6")+"',gjby='"+pi.getGjby()+"', jbgz='"+pi.getJbgz()+"',yfgz='"+df.format(yfgz)+"',zgz='"+df.format(zgz)+"'  where yf='"+pi.getYf()+"' and xm='"+nameBuffer+"' and sgxm='"+sgxmBuffer+"' and gd='"+dpBuffer+"' ");
 							flag=true;
 							alreadylist.remove(p);
 							break;
@@ -118,9 +122,12 @@ public class SgxmDaoImpl extends BaseDao<SgxmInfo> implements SgxmDao {
 				}
 				if(!flag){
 //					System.out.println("新增项目工资信息");
+					float zgs=(float)list.get(q).get("t5");
 					//计算应发工资=总工时*工价包月
-					double yfgz=((double)list.get(q).get("t5"))*Float.parseFloat(pi.getGjby());
-					this.executeUpdateOrDelete("insert into jl_sgxm_tj_info (id,xm,yf,gd,chuqin,jiaban,zonggongshi,gjby,jbgz,jbgzhjj,yfgz,lhbt,fybt,mq,qtkk,zgz,yfgzy,sygz,sgxm) values(UUID(),'"+nameBuffer+"','"+pi.getYf()+"','"+dpBuffer+"','"+list.get(q).get("t3")+"','"+list.get(q).get("t4")+"','"+list.get(q).get("t5")+"','"+pi.getGjby()+"','"+pi.getJbgz()+"','0','"+df.format(yfgz)+"','"+list.get(q).get("t6")+"','0','0','0','0','0','0','"+sgxmBuffer+"' )");
+					float yfgz=zgs*Float.parseFloat(pi.getGjby());
+					//总工资平均分到每个工时里面去的总工时 用每个小时的工资*小时数
+					float zgz =zgs*ehgz;
+					this.executeUpdateOrDelete("insert into jl_sgxm_tj_info (id,xm,yf,gd,chuqin,jiaban,zonggongshi,gjby,jbgz,jbgzhjj,yfgz,lhbt,fybt,mq,qtkk,zgz,yfgzy,sygz,sgxm) values(UUID(),'"+nameBuffer+"','"+pi.getYf()+"','"+dpBuffer+"','"+list.get(q).get("t3")+"','"+list.get(q).get("t4")+"','"+list.get(q).get("t5")+"','"+pi.getGjby()+"','"+pi.getJbgz()+"','0','"+df.format(yfgz)+"','"+list.get(q).get("t6")+"','0','0','0','"+df.format(zgz)+"','0','0','"+sgxmBuffer+"' )");
 				}
 			}
 			StringBuffer ids=new StringBuffer(200);
